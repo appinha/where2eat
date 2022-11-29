cb339103bd3babfc8dc1e236b366faeedf99cf26
cacauisadog <cbcaue@protonmail.com>
2022-11-28 21:21:28 -0300

    [back] Creates an application factory

---
+++ back/where2eat/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "where2eat.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+    @app.route("/")
+    def hello_world():
+        return jsonify({
+            'name': 'Osteria Itália',
+            'category': 'Italiana',
+            'openingHours': 'ter-sáb das 19h às 23h',
+            'address': 'R. Gen. Osório, 63 - Vila Ema',
+            'phone': '(12) 3308-0633',
+        })
+
+    return app
